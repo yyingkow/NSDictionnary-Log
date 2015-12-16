2d87cd596d91c8c7b02a18d526f78959ac9212ee
yyingkow <yyingkow@hotmail.com>
2015-12-17 00:48:58 +0800

    上传完整代码

---
+++ NSDictionary+Log.h
@@ -0,0 +1,24 @@
+//
+//  NSDictionary+Log.h
+//  NSDictionary+Log
+//
+//  Created by rk on 15/12/17.
+//  Copyright © 2015年 kyy. All rights reserved.
+//
+
+/**
+ 
+ 导入此文件后, 显示台可以直接显示出字典汉字内容
+ 
+ */
+
+#import <Foundation/Foundation.h>
+
+@interface NSDictionary (Log)
+
+@end
+
+
+@interface NSArray (Log)
+
+@end
---
+++ NSDictionary+Log.m
@@ -0,0 +1,48 @@
+//
+//  NSDictionary+Log.m
+//  NSDictionary+Log
+//
+//  Created by rk on 15/12/17.
+//  Copyright © 2015年 kyy. All rights reserved.
+//
+
+#import "NSDictionary+Log.h"
+
+@implementation NSDictionary (Log)
+
+- (NSString *)descriptionWithLocale:(id)locale
+{
+    NSMutableString *strM = [NSMutableString stringWithString:@"{\n"];
+    
+    [self enumerateKeysAndObjectsUsingBlock:^(id key, id obj, BOOL *stop) {
+        [strM appendFormat:@"\t%@ = %@;\n", key, obj];
+    }];
+    
+    [strM appendString:@"}\n"];
+    
+    return strM;
+}
+
+@end
+
+
+@implementation NSArray (Log)
+
+- (NSString *)descriptionWithLocale:(id)locale
+{
+    NSMutableString *strM = [NSMutableString stringWithString:@"(\n"];
+    
+    [self enumerateObjectsUsingBlock:^(id obj, NSUInteger idx, BOOL *stop) {
+        [strM appendFormat:@"\t%@,\n", obj];
+    }];
+    
+    [strM appendString:@")"];
+    
+    return strM;
+}
+
+@end
+
+
+
+
